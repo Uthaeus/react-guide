2d5f611fa83ce1a5076cbc7b67a3b85a36f1370d
Uthaeus <romanlavery@gmail.com>
2018-08-17 10:09:35 -0600

    Building dynamic components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+
+        <Person name="Max" age="28" />
+        <Person name="Manu" age="29">My Hobbies: Racing</Person>
+        <Person name="Stephanie" age="26" />
       </div>
     );
   }
