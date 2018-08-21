8f5f1badeabbee33e645858b239c23b0cd995fb3
Uthaeus <romanlavery@gmail.com>
2018-08-21 15:43:41 -0600

    Component creation lifecycle

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,15 +4,29 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
-  state = {
-    persons: [
-      {id: 'fadf', name: 'Max', age: 28 }, 
-      {id: 'asdf', name: 'Manu', age: 29 },
-      {id: 'jfal', name: 'Stephanie', age: 26 }
-    ],
-    otherState: 'some other value',
-    showPersons: false 
-  } 
+  constructor(props) {
+    super(props);
+    console.log('[App.js] Inside constructor', props);
+    this.state = {
+      persons: [
+        {id: 'fadf', name: 'Max', age: 28 }, 
+        {id: 'asdf', name: 'Manu', age: 29 },
+        {id: 'jfal', name: 'Stephanie', age: 26 }
+      ],
+      otherState: 'some other value',
+      showPersons: false 
+    }
+  }
+
+  componentWillMount() {
+    console.log('[App.js] Inside componentWillMount');
+  }
+
+  componentDidMount() {
+    console.log('[App.js] Inside componentDidMount')
+  }
+
+   
 
   deletePersonHandler = (personIndex) => {
     // const persons = this.state.persons.slice();
@@ -46,6 +60,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] Inside render');
 
     let persons = null;
 
