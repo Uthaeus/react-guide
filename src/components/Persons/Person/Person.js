77cbcf0ec5ecd36d33e2be13bbbac6b0272f1c1f
Uthaeus <romanlavery@gmail.com>
2018-08-22 11:09:56 -0600

    Using references

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -16,7 +16,11 @@ class Person extends Component {
   }
 
   componentDidMount() {
-    console.log('[Person.js] Inside componentDidMount')
+    console.log('[Person.js] Inside componentDidMount');
+    if (this.props.position === 0) {
+      this.inputElement.focus();
+    }
+    // directs focus on first element
   }
 
   render() {
@@ -25,7 +29,11 @@ class Person extends Component {
       <Aux>
         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old.</p>
         <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name} />
+        <input 
+          ref={(inp) => { this.inputElement = inp }}
+          type="text" 
+          onChange={this.props.changed} 
+          value={this.props.name} />
       </Aux>
     )
   }
